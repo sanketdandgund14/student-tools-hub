32128b6871044730ed07720d50cdc1e42f2ba72d
Sanket Dandgund <dandgundsanket@gmail.com>
2026-04-25 16:46:53 +0530

    fixed duplicate RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,15 +28,6 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
-      <body>{children}</body>
-    </html>
-  );
-}
-import Navbar from "./components/Navbar";
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
       <body>
         <Navbar />
         {children}
