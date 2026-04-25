02f2da329981b6266092f25fc0b495aef49c1588
Sanket Dandgund <dandgundsanket@gmail.com>
2026-04-25 16:48:37 +0530

    fixed sgpa page export

--- app/sgpa/page.tsx
+++ app/sgpa/page.tsx
@@ -1,1 +1,11 @@
-<Link href="/sgpa">Try SGPA Calculator</Link>
+"use client";
+
+export default function SGPA() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        SGPA Calculator (Coming Soon)
+      </h1>
+    </div>
+  );
+}
